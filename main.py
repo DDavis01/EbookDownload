d6e49972c35e4502be3eb9c446377e87e6ee1fe9
DDavis01 <drew.davis391998@gmail.com>
2020-01-06 14:09:01 -0500

    Added CSV Parsing, Started Documentation

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import utility
 
 BookInfo = ['0133594149','Computer Networking: A Top-Down Approach','Kurose & Ross,','2016']
 BookInfo2 =['9780073529608','Microelectronic Circuit Design ','Jaeger & Blalock','2015']
-link = utility.search(BookInfo2,1)
-utility.downloadBook(BookInfo2,link)
+CSV = 'textbooks.csv'
+BookList = utility.parseBookInfo(CSV)
+link = utility.search(BookList[0],1)
+utility.downloadBook(BookList[0],link)
--- utility.py
+++ utility.py
@@ -1,37 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
-import os
 import wget
-
+import csv
 #LibGenURL = 'http://gen.lib.rus.ec/'
 #Example URL = http://gen.lib.rus.ec/search.php?req=0133594149&open=0&res=25&view=simple&phrase=1&column=def
 
 
-def search(BookInfo,InfoType):
-    InfoType = 1
+def search(BookInfo,InfoType):    #Function to parse a list "BookInfo" and extract from Lib Gen the download link of the top selection based off of ISBN
     ISBN = BookInfo[0]
     Name = BookInfo[1]
     Author = BookInfo[2]
     Year = BookInfo[3]
     if InfoType == 1:
         URL = 'http://gen.lib.rus.ec/search.php?req='+ISBN+'&open=0&res=25&view=simple&phrase=1&column=identifier'
-
     response = requests.get(URL)
-    #print(response.text)
     html_soup = BeautifulSoup(response.text, 'lxml')
     links = []
     for a in html_soup.find_all('a', id=True):
         links.append(a['href'])
-    #print(links)
     selectResponse = requests.get('http://gen.lib.rus.ec/'+links[0])
     select_html_soup = BeautifulSoup(selectResponse.text, 'lxml')
     select_links=[]
     for a in select_html_soup.find_all('a', title='Gen.lib.rus.ec'):
         select_links.append(a['href'])
-    #print(select_links[0])
     return select_links[0]
 
-def downloadBook(BookInfo,link):
+def downloadBook(BookInfo,link):    #Function to download book given BookInfo list and the designated link
     ip = link.split('/')
     response = requests.get(link)
     html_soup = BeautifulSoup(response.text, 'lxml')
@@ -39,6 +33,11 @@ def downloadBook(BookInfo,link):
     bookLoc=dl_link['href']
     URL = 'http://'+ip[2]+bookLoc
     fileName = BookInfo[1]+".pdf"
-    print(URL)
-    filename = wget.download(URL)
+    filename = wget.download(URL,out=fileName)
     
+def parseBookInfo(CSV):
+    with open(CSV, 'r') as f:
+        reader = csv.reader(f)
+        bookList = list(reader)
+    #print(bookList)
+    return(bookList)
